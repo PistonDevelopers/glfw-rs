b824c6e022d546f2aa91414a7aae981a078d19d2
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-09-12 22:59:34 +1000

    Create examples folder

--- examples/window.rs
+++ examples/window.rs
@@ -3,18 +3,18 @@ use glfw3;
 
 import glfw3::*;
 
-fn main () {
+fn main() {
     
     if (glfwInit() == 0) {
         fail(~"glfwInit() failed\n");
     }
     
     let mut window = glfwCreateWindow(800, 600, GLFW_WINDOWED, ~"Hello, I am a window.");
-    if (window.ptr as bool == false) {
+    if (window.ptr == ptr::null()) {
         fail(~"glfwOpenWindow() failed\n");
     }
 
-    let mut done = false; 
+    let mut done = false;
     
     while (!done) {
         if (glfwGetKey(&mut window, GLFW_KEY_ESC) == GLFW_PRESS || !glfwGetWindowParam(&mut window, GLFW_CLOSE_REQUESTED) as bool) {
