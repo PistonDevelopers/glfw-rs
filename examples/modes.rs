69fb873739854390ec3e001ab61fc03cdfc8bd34
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-10-01 16:59:13 +1000

    Change argument to pointer

--- examples/modes.rs
+++ examples/modes.rs
@@ -8,19 +8,19 @@ fn main() {
     }
     
     let dt_mode = glfwGetDesktopMode();
-    io::println(~"Desktop mode:\n" + mode_str(dt_mode));
+    io::println(~"Desktop mode:\n" + mode_str(&dt_mode));
     
     let modes = glfwGetVideoModes();
     
     io::println(~"Available modes:");
-    modes.map(|m| { io::println(mode_str(m)) });
+    modes.map(|m| io::println(mode_str(m)));
     
     glfwTerminate();
 }
 
-fn mode_str(mode: GLFWvidmode) -> ~str {
-    fmt!("%d x %d\t%d (%d %d %d)",
-         mode.width as int, mode.height as int,
-         (mode.redBits + mode.blueBits + mode.greenBits) as int,
-         mode.redBits as int, mode.blueBits as int, mode.greenBits as int)
+fn mode_str(mode: &GLFWvidmode) -> ~str {
+    fmt!("%? x %?\t%? (%? %? %?)",
+         mode.width, mode.height,
+         (mode.redBits + mode.blueBits + mode.greenBits),
+         mode.redBits, mode.blueBits, mode.greenBits)
 }
