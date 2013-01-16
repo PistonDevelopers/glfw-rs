115d60fb984afc4616372de3c7c6a0978db1485d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-01-17 07:20:12 +1100

    get_version returns a struct instead of a tuple

--- examples/version.rs
+++ examples/version.rs
@@ -4,8 +4,8 @@ extern mod glfw3;
 fn main() {
     
     // get version tuple
-    let (major, minor, rev) = glfw3::get_version();
-    io::println(fmt!("(%d, %d, %d)", major, minor, rev));
+    let version = glfw3::get_version();
+    io::println(fmt!("(%d, %d, %d)", version.major, version.minor, version.rev));
     
     // get version string
     io::println(~"GLFW version: " + glfw3::get_version_string());
