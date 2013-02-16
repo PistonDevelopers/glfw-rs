541efe3c84a4160328426c9f7bf17a2a23465fa8
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-02-16 12:14:02 +1100

    Improve manual-init example

--- examples/manual-init.rs
+++ examples/manual-init.rs
@@ -4,37 +4,39 @@ fn main() {
     // Run this task on the main thread. Unlike C or C++, a Rust program
     // automatically starts a new thread, so this line is _essential_ to ensure
     // that the OS is able to update the window and recieve events from the user.
-    do task::task().sched_mode(task::PlatformThread).spawn {
+    do task::task().sched_mode(task::PlatformThread).spawn  {
+        use core::private::finally::Finally;
         
-        glfw::set_error_callback(error_callback);
-        
-        if !glfw::init() {
-            glfw::terminate();
-            die!(~"Failed to initialize GLFW\n");
-        }
-        
-        let window =
-            match glfw::Window::create(300, 300, "Hello this is window", glfw::Windowed) {
-                Some(w) => { w }
-                None => {
-                    glfw::terminate();
-                    die!(~"Failed to open GLFW window");
-                }
-            };
-        
-        window.make_context_current();
-        
-        let mut done = false;
-        
-        while !done {
-            glfw::poll_events();
-            if (window.get_key(glfw::KEY_ESC) == glfw::PRESS || window.get_param(glfw::SHOULD_CLOSE) != 0) {
-                done = true;
+        do (|| {
+            
+            glfw::set_error_callback(error_callback);
+            
+            if !glfw::init() {
+                die!(~"Failed to initialize GLFW\n");
             }
+            
+            let window =
+                match glfw::Window::create(300, 300, "Hello this is window", glfw::Windowed) {
+                    Some(w) => w,
+                    None    => die!(~"Failed to open GLFW window")
+                };
+            
+            window.make_context_current();
+            
+            let mut done = false;
+            
+            while !done {
+                glfw::poll_events();
+                if (window.get_key(glfw::KEY_ESC) == glfw::PRESS || window.get_param(glfw::SHOULD_CLOSE) != 0) {
+                    done = true;
+                }
+            }
+            
+            window.destroy();
+            
+        }).finally {
+            glfw::terminate();    // terminate glfw on completion
         }
-        
-        window.destroy();
-        glfw::terminate();
     }
 }
 
