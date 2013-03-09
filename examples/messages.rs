d89823ddcf9c9c0563f50e3af6aa2f0ce4e70735
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-03-10 01:30:34 +1100

    Use `msg.window` instead of `window`

--- examples/messages.rs
+++ examples/messages.rs
@@ -10,17 +10,17 @@ fn main() {
         
         do window.spawn_pos_listener |msg| {
             io::println(fmt!("Window pos: [%d, %d]", msg.x, msg.y));
-            // window.set_title(fmt!("Window pos: [%d, %d]", msg.x, msg.y));
+            // msg.window.set_title(fmt!("Window pos: [%d, %d]", msg.x, msg.y));
         }
         
         do window.spawn_key_listener |msg| {
             if msg.action == glfw::PRESS {
                 match msg.key {
                     k if k == glfw::KEY_ESCAPE => { window.set_should_close(true); }
-                    k if k == glfw::KEY_UP     => { move_window(&window, 0, 5);    }
-                    k if k == glfw::KEY_DOWN   => { move_window(&window, 0,-5);    }
-                    k if k == glfw::KEY_LEFT   => { move_window(&window,-5, 0);    }
-                    k if k == glfw::KEY_RIGHT  => { move_window(&window, 5, 0);    }
+                    k if k == glfw::KEY_UP     => { move_window(&msg.window, 0, 5);    }
+                    k if k == glfw::KEY_DOWN   => { move_window(&msg.window, 0,-5);    }
+                    k if k == glfw::KEY_LEFT   => { move_window(&msg.window,-5, 0);    }
+                    k if k == glfw::KEY_RIGHT  => { move_window(&msg.window, 5, 0);    }
                     _ => {}
                 }
             }
