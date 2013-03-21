64fba6777348939d0306c27823692ebb7289a88f
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-03-22 10:45:36 +1100

    Take advantage of fixes to cross-crate constant matching

--- examples/callbacks.rs
+++ examples/callbacks.rs
@@ -106,147 +106,147 @@ fn scroll_callback(window: &glfw::Window, x: f64, y: f64) {
 
 fn to_action_str(state: libc::c_int) -> ~str {
     match state {
-        s if s == glfw::RELEASE => { ~"Released" }
-        s if s == glfw::PRESS   => { ~"Pressed"  }
-        s if s == glfw::REPEAT  => { ~"Repeated" }
-        _                       => { ~"Unknown"  }
+        glfw::RELEASE => { ~"Released" }
+        glfw::PRESS   => { ~"Pressed"  }
+        glfw::REPEAT  => { ~"Repeated" }
+        _             => { ~"Unknown"  }
     }
 }
 
 fn to_key_str(key: libc::c_int) -> ~str {
     match key {
         /* Printable keys */
-        k if k == glfw::KEY_SPACE             => { ~"Space"         }
-        k if k == glfw::KEY_APOSTROPHE        => { ~"Apostrophe"    }
-        k if k == glfw::KEY_COMMA             => { ~"Comma"         }
-        k if k == glfw::KEY_MINUS             => { ~"Minus"         }
-        k if k == glfw::KEY_PERIOD            => { ~"Period"        }
-        k if k == glfw::KEY_SLASH             => { ~"Slash"         }
-        k if k == glfw::KEY_0                 => { ~"0"             }
-        k if k == glfw::KEY_1                 => { ~"1"             }
-        k if k == glfw::KEY_2                 => { ~"2"             }
-        k if k == glfw::KEY_3                 => { ~"3"             }
-        k if k == glfw::KEY_4                 => { ~"4"             }
-        k if k == glfw::KEY_5                 => { ~"5"             }
-        k if k == glfw::KEY_6                 => { ~"6"             }
-        k if k == glfw::KEY_7                 => { ~"7"             }
-        k if k == glfw::KEY_8                 => { ~"8"             }
-        k if k == glfw::KEY_9                 => { ~"9"             }
-        k if k == glfw::KEY_SEMICOLON         => { ~"Semicolon"     }
-        k if k == glfw::KEY_EQUAL             => { ~"Equal"         }
-        k if k == glfw::KEY_A                 => { ~"A"             }
-        k if k == glfw::KEY_B                 => { ~"B"             }
-        k if k == glfw::KEY_C                 => { ~"C"             }
-        k if k == glfw::KEY_D                 => { ~"D"             }
-        k if k == glfw::KEY_E                 => { ~"E"             }
-        k if k == glfw::KEY_F                 => { ~"F"             }
-        k if k == glfw::KEY_G                 => { ~"G"             }
-        k if k == glfw::KEY_H                 => { ~"H"             }
-        k if k == glfw::KEY_I                 => { ~"I"             }
-        k if k == glfw::KEY_J                 => { ~"J"             }
-        k if k == glfw::KEY_K                 => { ~"K"             }
-        k if k == glfw::KEY_L                 => { ~"L"             }
-        k if k == glfw::KEY_M                 => { ~"M"             }
-        k if k == glfw::KEY_N                 => { ~"N"             }
-        k if k == glfw::KEY_O                 => { ~"O"             }
-        k if k == glfw::KEY_P                 => { ~"P"             }
-        k if k == glfw::KEY_Q                 => { ~"Q"             }
-        k if k == glfw::KEY_R                 => { ~"R"             }
-        k if k == glfw::KEY_S                 => { ~"S"             }
-        k if k == glfw::KEY_T                 => { ~"T"             }
-        k if k == glfw::KEY_U                 => { ~"U"             }
-        k if k == glfw::KEY_V                 => { ~"V"             }
-        k if k == glfw::KEY_W                 => { ~"W"             }
-        k if k == glfw::KEY_X                 => { ~"X"             }
-        k if k == glfw::KEY_Y                 => { ~"Y"             }
-        k if k == glfw::KEY_Z                 => { ~"Z"             }
-        k if k == glfw::KEY_LEFT_BRACKET      => { ~"Left Bracket"  }
-        k if k == glfw::KEY_BACKSLASH         => { ~"Backslash"     }
-        k if k == glfw::KEY_RIGHT_BRACKET     => { ~"Right Bracket" }
-        k if k == glfw::KEY_GRAVE_ACCENT      => { ~"Grave Accent"  }
-        k if k == glfw::KEY_WORLD_1           => { ~"World 1"       }
-        k if k == glfw::KEY_WORLD_2           => { ~"World 2"       }
+        glfw::KEY_SPACE             => { ~"Space"         }
+        glfw::KEY_APOSTROPHE        => { ~"Apostrophe"    }
+        glfw::KEY_COMMA             => { ~"Comma"         }
+        glfw::KEY_MINUS             => { ~"Minus"         }
+        glfw::KEY_PERIOD            => { ~"Period"        }
+        glfw::KEY_SLASH             => { ~"Slash"         }
+        glfw::KEY_0                 => { ~"0"             }
+        glfw::KEY_1                 => { ~"1"             }
+        glfw::KEY_2                 => { ~"2"             }
+        glfw::KEY_3                 => { ~"3"             }
+        glfw::KEY_4                 => { ~"4"             }
+        glfw::KEY_5                 => { ~"5"             }
+        glfw::KEY_6                 => { ~"6"             }
+        glfw::KEY_7                 => { ~"7"             }
+        glfw::KEY_8                 => { ~"8"             }
+        glfw::KEY_9                 => { ~"9"             }
+        glfw::KEY_SEMICOLON         => { ~"Semicolon"     }
+        glfw::KEY_EQUAL             => { ~"Equal"         }
+        glfw::KEY_A                 => { ~"A"             }
+        glfw::KEY_B                 => { ~"B"             }
+        glfw::KEY_C                 => { ~"C"             }
+        glfw::KEY_D                 => { ~"D"             }
+        glfw::KEY_E                 => { ~"E"             }
+        glfw::KEY_F                 => { ~"F"             }
+        glfw::KEY_G                 => { ~"G"             }
+        glfw::KEY_H                 => { ~"H"             }
+        glfw::KEY_I                 => { ~"I"             }
+        glfw::KEY_J                 => { ~"J"             }
+        glfw::KEY_K                 => { ~"K"             }
+        glfw::KEY_L                 => { ~"L"             }
+        glfw::KEY_M                 => { ~"M"             }
+        glfw::KEY_N                 => { ~"N"             }
+        glfw::KEY_O                 => { ~"O"             }
+        glfw::KEY_P                 => { ~"P"             }
+        glfw::KEY_Q                 => { ~"Q"             }
+        glfw::KEY_R                 => { ~"R"             }
+        glfw::KEY_S                 => { ~"S"             }
+        glfw::KEY_T                 => { ~"T"             }
+        glfw::KEY_U                 => { ~"U"             }
+        glfw::KEY_V                 => { ~"V"             }
+        glfw::KEY_W                 => { ~"W"             }
+        glfw::KEY_X                 => { ~"X"             }
+        glfw::KEY_Y                 => { ~"Y"             }
+        glfw::KEY_Z                 => { ~"Z"             }
+        glfw::KEY_LEFT_BRACKET      => { ~"Left Bracket"  }
+        glfw::KEY_BACKSLASH         => { ~"Backslash"     }
+        glfw::KEY_RIGHT_BRACKET     => { ~"Right Bracket" }
+        glfw::KEY_GRAVE_ACCENT      => { ~"Grave Accent"  }
+        glfw::KEY_WORLD_1           => { ~"World 1"       }
+        glfw::KEY_WORLD_2           => { ~"World 2"       }
 
         /* Function keys */
-        k if k == glfw::KEY_ESCAPE            => { ~"Escape"        }
-        k if k == glfw::KEY_ENTER             => { ~"Enter"         }
-        k if k == glfw::KEY_TAB               => { ~"Tab"           }
-        k if k == glfw::KEY_BACKSPACE         => { ~"Backspace"     }
-        k if k == glfw::KEY_INSERT            => { ~"Insert"        }
-        k if k == glfw::KEY_DELETE            => { ~"Delete"        }
-        k if k == glfw::KEY_RIGHT             => { ~"Right"         }
-        k if k == glfw::KEY_LEFT              => { ~"Left"          }
-        k if k == glfw::KEY_DOWN              => { ~"Down"          }
-        k if k == glfw::KEY_UP                => { ~"Up"            }
-        k if k == glfw::KEY_PAGE_UP           => { ~"Page Up"       }
-        k if k == glfw::KEY_PAGE_DOWN         => { ~"Page Down"     }
-        k if k == glfw::KEY_HOME              => { ~"Home"          }
-        k if k == glfw::KEY_END               => { ~"End"           }
-        k if k == glfw::KEY_CAPS_LOCK         => { ~"Caps Lock"     }
-        k if k == glfw::KEY_SCROLL_LOCK       => { ~"Scroll Lock"   }
-        k if k == glfw::KEY_NUM_LOCK          => { ~"Num Lock"      }
-        k if k == glfw::KEY_PRINT_SCREEN      => { ~"Print Screen"  }
-        k if k == glfw::KEY_PAUSE             => { ~"Pause"         }
-        k if k == glfw::KEY_F1                => { ~"F1"            }
-        k if k == glfw::KEY_F2                => { ~"F2"            }
-        k if k == glfw::KEY_F3                => { ~"F3"            }
-        k if k == glfw::KEY_F4                => { ~"F4"            }
-        k if k == glfw::KEY_F5                => { ~"F5"            }
-        k if k == glfw::KEY_F6                => { ~"F6"            }
-        k if k == glfw::KEY_F7                => { ~"F7"            }
-        k if k == glfw::KEY_F8                => { ~"F8"            }
-        k if k == glfw::KEY_F9                => { ~"F9"            }
-        k if k == glfw::KEY_F10               => { ~"F10"           }
-        k if k == glfw::KEY_F11               => { ~"F11"           }
-        k if k == glfw::KEY_F12               => { ~"F12"           }
-        k if k == glfw::KEY_F13               => { ~"F13"           }
-        k if k == glfw::KEY_F14               => { ~"F14"           }
-        k if k == glfw::KEY_F15               => { ~"F15"           }
-        k if k == glfw::KEY_F16               => { ~"F16"           }
-        k if k == glfw::KEY_F17               => { ~"F17"           }
-        k if k == glfw::KEY_F18               => { ~"F18"           }
-        k if k == glfw::KEY_F19               => { ~"F19"           }
-        k if k == glfw::KEY_F20               => { ~"F20"           }
-        k if k == glfw::KEY_F21               => { ~"F21"           }
-        k if k == glfw::KEY_F22               => { ~"F22"           }
-        k if k == glfw::KEY_F23               => { ~"F23"           }
-        k if k == glfw::KEY_F24               => { ~"F24"           }
-        k if k == glfw::KEY_F25               => { ~"F25"           }
-        k if k == glfw::KEY_KP_0              => { ~"Kp 0"          }
-        k if k == glfw::KEY_KP_1              => { ~"Kp 1"          }
-        k if k == glfw::KEY_KP_2              => { ~"Kp 2"          }
-        k if k == glfw::KEY_KP_3              => { ~"Kp 3"          }
-        k if k == glfw::KEY_KP_4              => { ~"Kp 4"          }
-        k if k == glfw::KEY_KP_5              => { ~"Kp 5"          }
-        k if k == glfw::KEY_KP_6              => { ~"Kp 6"          }
-        k if k == glfw::KEY_KP_7              => { ~"Kp 7"          }
-        k if k == glfw::KEY_KP_8              => { ~"Kp 8"          }
-        k if k == glfw::KEY_KP_9              => { ~"Kp 9"          }
-        k if k == glfw::KEY_KP_DECIMAL        => { ~"Kp Decimal"    }
-        k if k == glfw::KEY_KP_DIVIDE         => { ~"Kp Divide"     }
-        k if k == glfw::KEY_KP_MULTIPLY       => { ~"Kp Multiply"   }
-        k if k == glfw::KEY_KP_SUBTRACT       => { ~"Kp Subtract"   }
-        k if k == glfw::KEY_KP_ADD            => { ~"Kp Add"        }
-        k if k == glfw::KEY_KP_ENTER          => { ~"Kp Enter"      }
-        k if k == glfw::KEY_KP_EQUAL          => { ~"Kp Equal"      }
-        k if k == glfw::KEY_LEFT_SHIFT        => { ~"Left Shift"    }
-        k if k == glfw::KEY_LEFT_CONTROL      => { ~"Left Control"  }
-        k if k == glfw::KEY_LEFT_ALT          => { ~"Left Alt"      }
-        k if k == glfw::KEY_LEFT_SUPER        => { ~"Left Super"    }
-        k if k == glfw::KEY_RIGHT_SHIFT       => { ~"Right Shift"   }
-        k if k == glfw::KEY_RIGHT_CONTROL     => { ~"Right Control" }
-        k if k == glfw::KEY_RIGHT_ALT         => { ~"Right Alt"     }
-        k if k == glfw::KEY_RIGHT_SUPER       => { ~"Right Super"   }
-        k if k == glfw::KEY_MENU              => { ~"Menu"          }
-        _                                      => { ~"Unknown"       }
+        glfw::KEY_ESCAPE            => { ~"Escape"        }
+        glfw::KEY_ENTER             => { ~"Enter"         }
+        glfw::KEY_TAB               => { ~"Tab"           }
+        glfw::KEY_BACKSPACE         => { ~"Backspace"     }
+        glfw::KEY_INSERT            => { ~"Insert"        }
+        glfw::KEY_DELETE            => { ~"Delete"        }
+        glfw::KEY_RIGHT             => { ~"Right"         }
+        glfw::KEY_LEFT              => { ~"Left"          }
+        glfw::KEY_DOWN              => { ~"Down"          }
+        glfw::KEY_UP                => { ~"Up"            }
+        glfw::KEY_PAGE_UP           => { ~"Page Up"       }
+        glfw::KEY_PAGE_DOWN         => { ~"Page Down"     }
+        glfw::KEY_HOME              => { ~"Home"          }
+        glfw::KEY_END               => { ~"End"           }
+        glfw::KEY_CAPS_LOCK         => { ~"Caps Lock"     }
+        glfw::KEY_SCROLL_LOCK       => { ~"Scroll Lock"   }
+        glfw::KEY_NUM_LOCK          => { ~"Num Lock"      }
+        glfw::KEY_PRINT_SCREEN      => { ~"Print Screen"  }
+        glfw::KEY_PAUSE             => { ~"Pause"         }
+        glfw::KEY_F1                => { ~"F1"            }
+        glfw::KEY_F2                => { ~"F2"            }
+        glfw::KEY_F3                => { ~"F3"            }
+        glfw::KEY_F4                => { ~"F4"            }
+        glfw::KEY_F5                => { ~"F5"            }
+        glfw::KEY_F6                => { ~"F6"            }
+        glfw::KEY_F7                => { ~"F7"            }
+        glfw::KEY_F8                => { ~"F8"            }
+        glfw::KEY_F9                => { ~"F9"            }
+        glfw::KEY_F10               => { ~"F10"           }
+        glfw::KEY_F11               => { ~"F11"           }
+        glfw::KEY_F12               => { ~"F12"           }
+        glfw::KEY_F13               => { ~"F13"           }
+        glfw::KEY_F14               => { ~"F14"           }
+        glfw::KEY_F15               => { ~"F15"           }
+        glfw::KEY_F16               => { ~"F16"           }
+        glfw::KEY_F17               => { ~"F17"           }
+        glfw::KEY_F18               => { ~"F18"           }
+        glfw::KEY_F19               => { ~"F19"           }
+        glfw::KEY_F20               => { ~"F20"           }
+        glfw::KEY_F21               => { ~"F21"           }
+        glfw::KEY_F22               => { ~"F22"           }
+        glfw::KEY_F23               => { ~"F23"           }
+        glfw::KEY_F24               => { ~"F24"           }
+        glfw::KEY_F25               => { ~"F25"           }
+        glfw::KEY_KP_0              => { ~"Kp 0"          }
+        glfw::KEY_KP_1              => { ~"Kp 1"          }
+        glfw::KEY_KP_2              => { ~"Kp 2"          }
+        glfw::KEY_KP_3              => { ~"Kp 3"          }
+        glfw::KEY_KP_4              => { ~"Kp 4"          }
+        glfw::KEY_KP_5              => { ~"Kp 5"          }
+        glfw::KEY_KP_6              => { ~"Kp 6"          }
+        glfw::KEY_KP_7              => { ~"Kp 7"          }
+        glfw::KEY_KP_8              => { ~"Kp 8"          }
+        glfw::KEY_KP_9              => { ~"Kp 9"          }
+        glfw::KEY_KP_DECIMAL        => { ~"Kp Decimal"    }
+        glfw::KEY_KP_DIVIDE         => { ~"Kp Divide"     }
+        glfw::KEY_KP_MULTIPLY       => { ~"Kp Multiply"   }
+        glfw::KEY_KP_SUBTRACT       => { ~"Kp Subtract"   }
+        glfw::KEY_KP_ADD            => { ~"Kp Add"        }
+        glfw::KEY_KP_ENTER          => { ~"Kp Enter"      }
+        glfw::KEY_KP_EQUAL          => { ~"Kp Equal"      }
+        glfw::KEY_LEFT_SHIFT        => { ~"Left Shift"    }
+        glfw::KEY_LEFT_CONTROL      => { ~"Left Control"  }
+        glfw::KEY_LEFT_ALT          => { ~"Left Alt"      }
+        glfw::KEY_LEFT_SUPER        => { ~"Left Super"    }
+        glfw::KEY_RIGHT_SHIFT       => { ~"Right Shift"   }
+        glfw::KEY_RIGHT_CONTROL     => { ~"Right Control" }
+        glfw::KEY_RIGHT_ALT         => { ~"Right Alt"     }
+        glfw::KEY_RIGHT_SUPER       => { ~"Right Super"   }
+        glfw::KEY_MENU              => { ~"Menu"          }
+        _                           => { ~"Unknown"       }
     }
 }
 
 fn to_mouse_button_str(btn: libc::c_int) -> ~str {
     match btn {
-        b if b == glfw::MOUSE_BUTTON_LEFT     => { ~"Left"     }
-        b if b == glfw::MOUSE_BUTTON_RIGHT    => { ~"Right"    }
-        b if b == glfw::MOUSE_BUTTON_MIDDLE   => { ~"Middle"   }
-        _                                      => { ~"Unknown"  }
+        glfw::MOUSE_BUTTON_LEFT     => { ~"Left"     }
+        glfw::MOUSE_BUTTON_RIGHT    => { ~"Right"    }
+        glfw::MOUSE_BUTTON_MIDDLE   => { ~"Middle"   }
+        _                           => { ~"Unknown"  }
     }
 }
