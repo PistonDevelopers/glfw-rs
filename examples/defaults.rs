39483feb28e9566100654fc07c86ee3484136519
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-04-02 14:06:00 +1100

    Add OpenGL parameter checks to 'defaults' example

--- examples/defaults.rs
+++ examples/defaults.rs
@@ -1,5 +1,9 @@
 extern mod glfw;
 
+fn error_callback(_error: libc::c_int, description: ~str) {
+    io::println(fmt!("GLFW Error: %s", description));
+}
+
 fn main() {
     glfw::set_error_callback(error_callback);
     
@@ -14,24 +18,71 @@ fn main() {
         let (width, height) = window.get_size();
         io::println(fmt!("window size: %? x %?", width, height));
         
-        io::println(fmt!("Context version major: %?",     window.get_param(glfw::CONTEXT_VERSION_MAJOR)));
-        io::println(fmt!("Context version minor: %?",     window.get_param(glfw::CONTEXT_VERSION_MINOR)));
-        io::println(fmt!("OpenGL forward compatible: %?", window.get_param(glfw::OPENGL_FORWARD_COMPAT)));
-        io::println(fmt!("OpenGL debug context: %?",      window.get_param(glfw::OPENGL_DEBUG_CONTEXT)));
-        io::println(fmt!("OpenGL profile: %?",            window.get_param(glfw::OPENGL_PROFILE)));
+        println(fmt!("Context version major: %?",     window.get_param(glfw::CONTEXT_VERSION_MAJOR)));
+        println(fmt!("Context version minor: %?",     window.get_param(glfw::CONTEXT_VERSION_MINOR)));
+        println(fmt!("OpenGL forward compatible: %?", window.get_param(glfw::OPENGL_FORWARD_COMPAT)));
+        println(fmt!("OpenGL debug context: %?",      window.get_param(glfw::OPENGL_DEBUG_CONTEXT)));
+        println(fmt!("OpenGL profile: %?",            window.get_param(glfw::OPENGL_PROFILE)));
         
-        // TODO: Test OpenGL defaults: 
-        //   - GL_RED_BITS
-        //   - GL_GREEN_BITS
-        //   - GL_BLUE_BITS
-        //   - GL_ALPHA_BITS
-        //   - GL_DEPTH_BITS
-        //   - GL_STENCIL_BITS
-        //   - GL_STEREO
-        //   - GL_SAMPLES_ARB
+        let gl_params = [
+            (gl::RED_BITS,          None,   "red bits"          ),
+            (gl::GREEN_BITS,        None,   "green bits"        ),
+            (gl::BLUE_BITS,         None,   "blue bits"         ),
+            (gl::ALPHA_BITS,        None,   "alpha bits"        ),
+            (gl::DEPTH_BITS,        None,   "depth bits"        ),
+            (gl::STENCIL_BITS,      None,   "stencil bits"      ),
+            (gl::ACCUM_RED_BITS,    None,   "accum red bits"    ),
+            (gl::ACCUM_GREEN_BITS,  None,   "accum green bits"  ),
+            (gl::ACCUM_BLUE_BITS,   None,   "accum blue bits"   ),
+            (gl::ACCUM_ALPHA_BITS,  None,   "accum alpha bits"  ),
+            (gl::STEREO,            None,   "stereo"            ),
+            (gl::SAMPLES_ARB,       Some("GL_ARB_multisample"), "FSAA samples" ),
+        ];
+        
+        for gl_params.each |&p| {
+            let (param, ext, name) = p;
+            
+            if do ext.map_default(true) |&s| {
+                glfw::extension_supported(s)
+            } {
+                let mut value = 0;
+                unsafe { gl::GetIntegerv(param, &value); }
+                
+                println(fmt!("OpenGL %s: %?", name, value));
+            };
+        }
     }
 }
 
-fn error_callback(_error: libc::c_int, description: ~str) {
-    io::println(fmt!("GLFW Error: %s", description));
+mod gl {
+    #[nolink]
+    #[link_args="-framework OpenGL"]
+    #[cfg(target_os = "macos")]
+    extern mod linkhack {}
+
+    #[nolink]
+    #[link_args="-lGL"]
+    #[cfg(target_os = "linux")]
+    extern mod linkhack {}
+    
+    pub type GLenum = libc::c_uint;
+    pub type GLint  = libc::c_int;
+    
+    pub static RED_BITS              : GLenum = 0x0D52;
+    pub static GREEN_BITS            : GLenum = 0x0D53;
+    pub static BLUE_BITS             : GLenum = 0x0D54;
+    pub static ALPHA_BITS            : GLenum = 0x0D55;
+    pub static DEPTH_BITS            : GLenum = 0x0D56;
+    pub static STENCIL_BITS          : GLenum = 0x0D57;
+    pub static ACCUM_RED_BITS        : GLenum = 0x0D58;
+    pub static ACCUM_GREEN_BITS      : GLenum = 0x0D59;
+    pub static ACCUM_BLUE_BITS       : GLenum = 0x0D5A;
+    pub static ACCUM_ALPHA_BITS      : GLenum = 0x0D5B;
+    pub static STEREO                : GLenum = 0x0C33;
+    pub static SAMPLES_ARB           : GLenum = 0x80A9;
+    
+    pub extern "C" {
+        #[link_name="glGetIntegerv"]
+        pub fn GetIntegerv(pname: GLenum, params: *GLint);
+    }
 }
