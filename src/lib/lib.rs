ad2d1fa5fa9a96f69ebcc66c90df683089a837b3
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-03-30 23:32:34 +1100

    Use new attribute syntax

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -13,15 +13,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[crate_type = "lib"];
-#[crate_type = "rlib"];
-#[crate_type = "dylib"];
-#[crate_id = "github.com/bjz/glfw-rs#glfw:0.1"];
-#[comment = "Bindings and wrapper functions for glfw3."];
+#![crate_type = "lib"]
+#![crate_type = "rlib"]
+#![crate_type = "dylib"]
+#![crate_id = "github.com/bjz/glfw-rs#glfw:0.1"]
+#![comment = "Bindings and wrapper functions for glfw3."]
 
-#[feature(globs)];
-#[feature(macro_rules)];
-#[feature(phase)];
+#![feature(globs)]
+#![feature(macro_rules)]
 
 // TODO: Document differences between GLFW and glfw-rs
 
