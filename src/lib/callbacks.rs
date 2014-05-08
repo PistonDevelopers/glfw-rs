24e9fe04722b06cabac7f2d6d3a6c6c3cb57063a
Hanno Braun <mail@hannobraun.de>
2014-05-08 19:21:25 +0000

    Bring code up to date with local_data API changes

--- src/lib/callbacks.rs
+++ src/lib/callbacks.rs
@@ -43,35 +43,33 @@ macro_rules! callback(
 
         #[cfg(not(target_word_size = "32"))]
         pub fn set<UserData: 'static>(f: ::$Callback<UserData>) {
-            ::std::local_data::set(CALLBACK_KEY, box f as Box<Object<Args>:'static>);
+            CALLBACK_KEY.replace(Some(box f as Box<Object<Args>:'static>));
             ($ext_set)(Some(callback));
         }
         // FIXME: workaround for mozilla/rust#11040
         #[cfg(target_word_size = "32")]
         pub fn set<UserData: 'static>(f: ::$Callback<UserData>) {
-            ::std::local_data::set(CALLBACK_KEY, box f as Box<Object<Args>:'static>);
+            CALLBACK_KEY.replace(Some(box f as Box<Object<Args>:'static>));
             ($ext_set)(callback);
         }
 
         #[cfg(not(target_word_size = "32"))]
         pub fn unset() {
-            ::std::local_data::pop(CALLBACK_KEY);
+            CALLBACK_KEY.replace(None);
             ($ext_set)(None);
         }
         // FIXME: workaround for mozilla/rust#11040
         #[cfg(target_word_size = "32")]
         pub fn unset() {
-            ::std::local_data::pop(CALLBACK_KEY);
+            CALLBACK_KEY.replace(None);
             ($ext_set)(unsafe { cast::transmute(::std::ptr::null::<::libc::c_void>()) });
         }
 
         extern "C" fn callback($($ext_arg: $ext_arg_ty),*) {
-            ::std::local_data::get(CALLBACK_KEY, |data| {
-                match data {
-                    Some(cb) => unsafe { cb.call($call) },
-                    _ => {}
-                }
-            });
+            match CALLBACK_KEY.get() {
+                Some(cb) => unsafe { cb.call($call) },
+                _ => {}
+            }
         }
     )
 )
